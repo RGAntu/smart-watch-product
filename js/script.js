c80455a7fee3b97838c50c006b591b17f78bca83
RGAntu <rgantu887@gmail.com>
2025-12-27 01:16:37 +0600

    create cartItems empty array & push data into cartItems

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ for(let i = 0; i < ringButtons.length; i++){
     
     ringBtn.addEventListener("click", function(event){
         const color = event.target.id.replace("-color", "")
-        console.log(color)
         
         for(let j = 0; j < ringButtons.length; j++){
             
@@ -31,7 +30,6 @@ function selectWristSize(size){
     const sizes = ["S", "M", "L", "XL"];
     for(let i = 0; i < sizes.length; i++){
         const button = document.getElementById("size-" + sizes[i]);
-        console.log(button)
         const element = sizes[i];
         if( size === element){
            button.classList.add("border-purple-600");
@@ -58,6 +56,7 @@ for(let btn of quantityElements){
 
 // add to cart
 let cartCount = 0;
+let cartItems = [];
 document.getElementById("add-to-cart").addEventListener("click", function(){
     const quantity = parseInt(document.getElementById("quantity").innerText);
 
@@ -65,6 +64,30 @@ document.getElementById("add-to-cart").addEventListener("click", function(){
         document.getElementById("checkout-container").classList.remove("hidden")
         cartCount = cartCount + quantity;
         document.getElementById("cart-count").innerText = cartCount;
+
+        const selectedColorButton = document.querySelector("button.border-purple-600.w-6");
+        const selectedColor = selectedColorButton.id.split("-")[0];
+
+        const selectedSizeButtons = document.querySelector("button.border-purple-600:not(.w-6)"
+
+        );
+
+        const selectedSize = selectedSizeButtons.innerText.split(" ")[0];
+
+        const selectedPrice = selectedSizeButtons.innerText.split(" ")[1].split("$")[1];
+
+        
+
+        cartItems.push({
+            image: selectedColor + ".png",
+            title: "Classy Modern Smart Watch",
+            color: selectedColor,
+            size: selectedSize,
+            price: quantity * parseInt(selectedPrice),
+        })
+        console.log(cartItems)
+        
+
     }
     else{
         alert("Please select a quantity...")
